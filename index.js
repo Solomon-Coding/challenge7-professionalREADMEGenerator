cbcdf09c1bd152e7a0d1f1ae8c86ffa1eac10e7d
Solomon Vana <117047239+Solomon-Coding@users.noreply.github.com>
2023-01-29 14:18:27 -0600

    added some structure to the data object

--- index.js
+++ index.js
@@ -1,7 +1,56 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+
+];
+
+inquirer
+.prompt([
+    {
+        type: 'input',
+        message: 'What is your name?',
+        name: 'name',
+    },
+    {
+        type: 'input',
+        message: 'What is your current location?',
+        name: 'location',
+    },
+    {
+        type: 'input',
+        message: 'Tell us about yourself.',
+        name: 'bio',
+    },
+    {
+        type: 'input',
+        message: 'What is your LinkedIn username?',
+        name: 'linkedInUser',
+    },
+    {
+        type: 'input',
+        message: 'What is your Github username?',
+        name: 'gitHubUser',
+    },
+])
+.then((data) => {
+    const prompt = {
+        title: data.title,
+        description: data.description,
+        contents: data.contents,
+        installation: data.installation,
+        usage: data.usage,
+        license: data.license,
+        contributing: data.contributing,
+        test: data.test,
+        questions: data.questions
+    }
+
+    const html = writeHTML(prompt);
+
+    // writeHTMLFile(html);
+
+});
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -13,7 +13,22 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
+  ## Description
+  # ${data.description}
+  ## Table of Contents
+  # ${data.contents}
+  ## Installation
+  # ${data.installation}
+  ## Usage
+  # ${data.usage}
+  ## License
+  # ${data.license}
+  ## Contributing
+  # ${data.contributing}
+  ## Tests
+  # ${data.test}
+  ## Questions
+  # ${data.questions}
 `;
 }
 
